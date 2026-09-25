bc62c0b90654666cd101302ca2b4b1073a538550
Andres Ferra <andres.ferra@mi.unc.edu.ar>
2026-09-25 18:26:34 -0300

    crear clase Contacto, pom.xml, inicio de implementacion de Directorio Telefonico

--- src/main/java/com/ulp/lab1_tp5/DirectorioTelefonico.java
+++ src/main/java/com/ulp/lab1_tp5/DirectorioTelefonico.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.ulp.lab1_tp5;
 
-/**
- *
- * @author andrew
- */
-public class Lab1_TP5 {
+public class DirectorioTelefonico {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");

e5490534972ccdee0bc2214f260582e7d5586187
Andres Ferra <andres.ferra@mi.unc.edu.ar>
2026-09-25 19:02:08 -0300

    implementación del metodo agregar contacto

--- src/main/java/com/ulp/lab1_tp5/DirectorioTelefonico.java
+++ src/main/java/com/ulp/lab1_tp5/DirectorioTelefonico.java
@@ -1,8 +1,25 @@
 package com.ulp.lab1_tp5;
 
+import java.util.TreeMap;
+
 public class DirectorioTelefonico {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        private TreeMap<Long, Contacto> contactos;
+
+    public DirectorioTelefonico() {
+        this.contactos = new TreeMap<>();
+    }
+    
+    public void agregarContacto(Long telefono, Contacto contacto) {
+        if ( telefono == null && contacto == null)
+            throw new IllegalArgumentException("Teléfono y contacto no pueden ser nulos");
+        
+        if (contactos.containsKey(telefono)) {
+            System.out.println("Ya existe un contacto con el teléfono " + telefono);
+            return;
+        }
+        
+        contactos.put(telefono, contacto);
     }
 }
